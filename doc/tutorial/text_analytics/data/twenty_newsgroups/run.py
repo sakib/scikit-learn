767c3db235aa6a47af693e150fbc26570bf53022
Sakib Jalal <sakib.jalal@gmail.com>
2017-09-27 20:21:39 -0400

    pre speedup

--- doc/tutorial/text_analytics/data/twenty_newsgroups/run.py
+++ doc/tutorial/text_analytics/data/twenty_newsgroups/run.py
@@ -1,11 +1,11 @@
 #!/ilab/users/sfj19/anaconda3/bin/python
-""" Script to test my shit """
+""" Script to test my stuff """
 
 # imports
-#import math
-import random
+import math
+#import random
 from collections import defaultdict
-from numpy import dot, array
+from numpy import dot, array, add
 from numpy.linalg import norm
 from scipy.sparse import csr_matrix
 from matplotlib import pyplot as plt
@@ -17,7 +17,7 @@ TWENTY_TRAIN = fetch_20newsgroups()
 COUNT_VECT = CountVectorizer()
 TFIDF_TRANSFORMER = TfidfTransformer()
 N_NEWSGROUPS = len(TWENTY_TRAIN.target_names)
-ONE_OUT_OF = 10000
+ONE_OUT_OF = 100
 
 
 # partition newsgroups
@@ -102,7 +102,7 @@ NORMS_TFIDF = get_row_norms(M_TFIDF)
 AVG_SCORES_TFIDF = [[-1 for x in range(N_NEWSGROUPS)] for x in range(N_NEWSGROUPS)]
 Q_GROUP_AVG_TFIDF = [[-1 for x in range(N_NEWSGROUPS)] for x in range(N_NEWSGROUPS)]
 
-# 2.3
+MODELS = [M_BAG, M_TOKEN, M_TFIDF]
 GROUPS = [partition[:100] for partition in DOCUMENT_PARTITION]
 QUERIES = [partition[100:len(partition)] for partition in DOCUMENT_PARTITION]
 THRESHOLDS = [i/10 for i in range(11)]
@@ -110,18 +110,43 @@ PRECISIONS_BAG, RECALLS_BAG = [[0 for i in range(11)] for x in range(2)]
 PRECISIONS_TOKEN, RECALLS_TOKEN = [[0 for i in range(11)] for x in range(2)]
 PRECISIONS_TFIDF, RECALLS_TFIDF = [[0 for i in range(11)] for x in range(2)]
 
+# Lemma:
+# [sum_i][sum_j] dot(v_i, w_j)/(norm(v_i)*norm(w_j)
+# = [sum_i] dot( v_i/norm(v_i), [sum_i] w_j/norm(w_j) )
+def marginal_unit_vector_sums(partition, models):
+    """
+    Input:
+        - groups: list of sublists, where each sublist contains doc vectors representing newsgroups
+        - queries: list of sublists, where each sublist contains doc vectors representing query docs
+        - models: list of models where each model has a different data vector representation
+    Output:
+        - summations: 2d list indexed first by data vector rep and second by newsgroup
+            each element is a vector yielded through summation over all unit vectors of a newsgroup
+    """
+    summations = [[0 for a in range(len(partition))] for b in range(len(models))]
 
+    for m_id in range(len(models)):
+        for n_id in range(len(partition)):
+            for doc_idx in partition[n_id]:
+                vector = models[m_id].getrow(doc_idx).toarray()[0]
+                summations[m_id][n_id] = add(summations[m_id][n_id], vector/norm(vector))
+
+    return summations
+
+print('Doing the thing!')
+
+"""
 # 2.3.2
 for t in range(len(THRESHOLDS)-1):
-    count = 0
+    count_bag, count_token, count_tfidf = [0 for x in range(3)]
     tol = THRESHOLDS[t]
     for newsgroup_id in range(0, len(QUERIES)):
-        NR = len(DOCUMENT_PARTITION[newsgroup_id])
         for query_idx in QUERIES[newsgroup_id]:
             # given a query and threshold, calculate average precision/recall over all newsgroups
             if random.randrange(0, ONE_OUT_OF) != 0:
                 continue
             dr_bag, dt_bag, dr_token, dt_token, dr_tfidf, dt_tfidf = [0 for x in range(6)]
+            NR = len(GROUPS[newsgroup_id])
             for group_id in range(0, len(GROUPS)):
                 for doc_idx in GROUPS[group_id]:
                     # bag of words
@@ -146,26 +171,28 @@ for t in range(len(THRESHOLDS)-1):
                         if group_id == newsgroup_id:
                             dr_tfidf += 1
             if dt_bag != 0:
-                count += 1
+                count_bag += 1
                 PRECISIONS_BAG[t] += dr_bag/dt_bag
             RECALLS_BAG[t] += dr_bag/NR
             if dt_token != 0:
-                count += 1
+                count_token += 1
                 PRECISIONS_TOKEN[t] += dr_token/dt_token
             RECALLS_TOKEN[t] += dr_token/NR
             if dt_tfidf != 0:
-                count += 1
+                count_tfidf += 1
                 PRECISIONS_TFIDF[t] += dr_tfidf/dt_tfidf
             RECALLS_TFIDF[t] += dr_tfidf/NR
-            print('count:', count, '| query:', query_idx, \
-                    '| newsgroup:', TWENTY_TRAIN.target_names[newsgroup_id], '| threshold:', tol)
-    if count != 0:
-        PRECISIONS_BAG[t] /= count
-        RECALLS_BAG[t] /= count
-        PRECISIONS_TOKEN[t] /= count
-        RECALLS_TOKEN[t] /= count
-        PRECISIONS_TFIDF[t] /= count
-        RECALLS_TFIDF[t] /= count
+            print('bag:', count_bag, '| token:', count_token, '| tfidf:', count_tfidf, '| query:', \
+            query_idx, '| newsgroup:', TWENTY_TRAIN.target_names[newsgroup_id], '| threshold:', tol)
+    if count_bag != 0:
+        PRECISIONS_BAG[t] /= count_bag
+        RECALLS_BAG[t] /= count_bag
+    if count_token != 0:
+        PRECISIONS_TOKEN[t] /= count_token
+        RECALLS_TOKEN[t] /= count_token
+    if count_tfidf != 0:
+        PRECISIONS_TFIDF[t] /= count_tfidf
+        RECALLS_TFIDF[t] /= count_tfidf
     print('bag of words: precisions', PRECISIONS_BAG)
     print('bag of words: recalls', RECALLS_BAG)
     print('tokenized: precisions', PRECISIONS_TOKEN)
@@ -196,9 +223,10 @@ plt.ylim([0.0, 1.05])
 plt.plot(RECALLS_TFIDF, PRECISIONS_TFIDF)
 plt.title('Precision-Recall Curve: TFIDF')
 plt.show()
+"""
 
 """
-# 2.3.1
+# 2.3.2
 for a in range(N_NEWSGROUPS):
     for b in range(N_NEWSGROUPS):
         newsgrp_a = TWENTY_TRAIN.target_names[a]
@@ -247,12 +275,21 @@ plt.imshow(Q_GROUP_AVG_TFIDF, cmap='hot', interpolation='nearest')
 plt.show()
 """
 
-""" 2.2
+# """
+# 2.2
+# Lemma
+# [sum_i][sum_j] dot(v_i, w_j)/(norm(v_i)*norm(w_j) =
+# [sum_i] dot( v_i/norm(v_i), [sum_i] w_j/norm(w_j) )
+print('Calculating marginal newsgroup unit vector summations...')
+SUMS_BAG, SUMS_TOKEN, SUMS_TFIDF = marginal_unit_vector_sums(DOCUMENT_PARTITION, MODELS)
+
 for a in range(N_NEWSGROUPS):
     for b in range(N_NEWSGROUPS):
         newsgrp_a = TWENTY_TRAIN.target_names[a]
         newsgrp_b = TWENTY_TRAIN.target_names[b]
-        iters = math.floor((len(DOCUMENT_PARTITION[a])*len(DOCUMENT_PARTITION[b]))/ONE_OUT_OF)
+
+        #iters = math.floor((len(DOCUMENT_PARTITION[a])*len(DOCUMENT_PARTITION[b]))/ONE_OUT_OF)
+        iters = math.floor(len(DOCUMENT_PARTITION[a]*len(DOCUMENT_PARTITION[b])))
         print('------------------------------------')
         print('newsgroups:', newsgrp_a, newsgrp_b)
         print('number of pairs:', iters)
@@ -260,12 +297,14 @@ for a in range(N_NEWSGROUPS):
         if AVG_SCORES_TOKEN[a][b] != -1:
             continue
 
-        cos_sum_bag = 0
-        cos_sum_token = 0
-        cos_sum_tfidf = 0
+        cos_sum_bag = dot(SUMS_BAG[a], SUMS_BAG[b])
+        cos_sum_token = dot(SUMS_TOKEN[a], SUMS_TOKEN[b])
+        cos_sum_tfidf = dot(SUMS_TFIDF[a], SUMS_TFIDF[b])
+
+        """ O(n^2)
         for doc_idx_a in DOCUMENT_PARTITION[a]:
             for doc_idx_b in DOCUMENT_PARTITION[b]:
-                # sample 10%
+                # sample
                 if random.randrange(0, ONE_OUT_OF) != 0:
                     continue
                 # bag of words
@@ -280,7 +319,7 @@ for a in range(N_NEWSGROUPS):
                 v_a = M_TFIDF.getrow(doc_idx_a).toarray()[0]
                 v_b = M_TFIDF.getrow(doc_idx_b).toarray()[0]
                 cos_sum_tfidf += cos_sim(v_a, doc_idx_a, v_b, doc_idx_b, NORMS_TFIDF)
-
+        """
         AVG_SCORES_BAG[a][b] = cos_sum_bag/iters
         AVG_SCORES_BAG[b][a] = cos_sum_bag/iters
         print('avg sim on bag rep:\t', cos_sum_bag/iters)
@@ -300,4 +339,4 @@ plt.show()
 
 plt.imshow(AVG_SCORES_TFIDF, cmap='hot', interpolation='nearest')
 plt.show()
-"""
+# """
